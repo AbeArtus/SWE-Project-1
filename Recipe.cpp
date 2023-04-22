c37eb7a444ffedccabb26035a93ac2065545acb8
jbn8 <jbnguyen@ou.edu>
2023-04-21 20:20:18 -0500

    Recipe methods added (not tested)

--- Recipe.cpp
+++ Recipe.cpp
@@ -17,6 +17,9 @@ class Recipe {
         string description;
         vector<Ingredient> Ingredients;
         string directions;
+        string servingSize;
+        int cookTime; // units in minutes
+
         
         
     string getIngredientString() {
@@ -44,18 +47,91 @@ class Recipe {
     }
 
     void updatelist(string name, string dir) {
-        
-    }
 
-    Recipe(string name, string dscr, vector<Ingredient> Ings, string dirctions, string dirctry) {
+        // get the list from the given directory
+        string direc =  "Recipes/"+dir+"/~list.txt";
+
+        ofstream directoryFile;
+        directoryFile.open(direc);
+        // check to see if the file is opened
+        if (directoryFile.is_open()){
+            // read the file to see if name exists
+            string recipe;
+            while (getline(directoryFile, recipe)){
+                int sameName = strcmp(name, recipe);
+                if (sameName ==0){
+                    // the recipe is already in the file
+                    cout<< " Recipe already exists"<< endl;
+                    break;
+                }
+            }
+            // Add the recipe name  if ! exists
+            directoryFile << name;
+
+        }
+        directoryFile.close();
+
+    }
+        
+
+
+    // Constructor
+
+    Recipe(string name, string serving, int time, string dscr, vector<Ingredient> Ings, string dirctions, string dirctry) {
         ofstream outfile;
         string flname = replace(name, ' ', '_');
         outfile.open("Recipes/"+dirctry+"/"+flname);
         outfile << "abeartus" << endl << "SWEproj00";
-       
-       
-       
+
         outfile.close();
+    }
+
+    // Create a textfile from the parameters in the constructor
+
+    writeTextFile() {
+
+        // create a text file
+
+        fstream recipeFile;
+        recipe_file.open(name, ios::out);
+        if !(recipeFile)
+        {
+            cout << "File not created";
+        }
+        else{
+            // Write to the file
+
+            // Write to the text file
+            recipeFile << name;
+            recipeFile << "#";
+            recipeFile << servingSize;
+            recipeFile << "#";
+            recipeFile << cookTime;
+            recipeFile << "#";
+            recipeFile << description;
+            recipeFile << "#";
+
+            // loop for instruction vector
+            int numIngr = Ingredients.size();
+            for (int idx = 0; i < numIngr; i++) {
+                Ingredient current = Ingredients[idx];
+                recipeFile << current;
+
+                if !(idx == numIngr - 1)
+                {
+                    // Add a new line if it is not the last ingredient in the vector
+                    recipeFile << endl;
+                }
+
+                recipeFile << "#";
+                recipeFile << directions;
+            }
+
+            // Close the file
+            recioeFile.come();
+
+
+        }
     }
 
     // directories /Recipes/{Breakfast}{Lunch}{Dinner}{Snacks}{Desert}
@@ -96,15 +172,16 @@ class Recipe {
         while(getline(infile, line)){
             directions.append(line + "\n");
         }
-
     }
 
-    
 
-         
-};
+}
+
+
     int main() {
         Recipe recipe = Recipe("Breakfast_Burritos", "Breakfast");
         cout << recipe.getString();
+
+        recipe.updatelist(recipe.name, "Breakfast")
         return 0;
     }
