1835dce613862c9962c2a6b41fec7f334528e018
Zane Lesley <zanelesley@ou.edu>
2023-04-23 18:59:58 -0500

    Login is now working does not pass user
    TODO: pass userinfo

--- The-Fridge/login.cpp
+++ The-Fridge/login.cpp
@@ -1,6 +1,9 @@
 #include "login.h"
 #include "ui_login.h"
 #include <string>
+#include <QMessageBox>
+#include <QFile>
+#include <QDir>
 
 login::login(QWidget *parent) :
     QDialog(parent),
@@ -14,13 +17,42 @@ login::~login()
 {
     delete ui;
 }
-void login::on_login_Button_released()
+
+void login::on_loginButton_clicked()
 {
-    // save the string in the line edit to usr
-    std:std::string usr = ui->usr_lnEdt->text().toStdString();
-    // save the password to psw
-    std::string psw = ui->Pss_lnEdt->text().toStdString();
+    //Save login information
+    QString username = ui->usr_lnEdt->text();
+    QString password = ui->Pss_lnEdt->text();
+    QString fileName = username + ".txt";
 
+    // Get the parent directory of the current directory
+    QDir parentDir(QCoreApplication::applicationDirPath());
+    parentDir.cdUp();
+
+    // Append the "usr" directory and the file name to the parent directory path
+    QString filePath = parentDir.filePath("The-Fridge/usr/" + fileName);
+    QFile file(filePath);
+
+    //Start Authentication process
+    if(file.exists()){
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            //Stream in text
+            QTextStream in(&file);
+            //Read first line
+            in.readLine();
+            //Reads second line
+            QString keyword = in.readLine();
+            file.close();
+            //If successful, switch to MainWindow
+            if(password == keyword){
+                hide();
+                mainWindow = new MainWindow(this);
+                mainWindow->show();
+            }
+        }
+    }
+    else {
+        QMessageBox::warning(this, "Login", "username or password is incorrect");
+    }
 }
-
 
