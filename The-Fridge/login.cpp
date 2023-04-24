f9b5782949c06504256f36442277c67431977298
Zane Lesley <zanelesley@ou.edu>
2023-04-23 19:09:29 -0500

    login update with all ZaneL Files
    Pushing the rest of the files i have for my branch

--- The-Fridge/login.cpp
+++ The-Fridge/login.cpp
@@ -18,6 +18,18 @@ login::~login()
     delete ui;
 }
 
+//TODO: This can be deleted? Corresponding code in other files to be deleted as well?
+void login::on_login_Button_released()
+{
+    // save the string in the line edit to usr
+    std:std::string usr = ui->usr_lnEdt->text().toStdString();
+    // save the password to psw
+    std::string psw = ui->Pss_lnEdt->text().toStdString();
+
+}
+
+
+
 void login::on_loginButton_clicked()
 {
     //Save login information
@@ -43,7 +55,7 @@ void login::on_loginButton_clicked()
             //Reads second line
             QString keyword = in.readLine();
             file.close();
-            //If successful, switch to MainWindow
+            //If successfu
             if(password == keyword){
                 hide();
                 mainWindow = new MainWindow(this);
--- unittest/UnitTest.cpp
+++
@@ -1,5 +0,0 @@
-#include <gtest/gtest.h>
-
-TEST(ExampleTest, DemonstrateGTestMacros){
-    EXPECT_TRUE(true);
-}
